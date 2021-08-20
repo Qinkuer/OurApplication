a795a04e6c2e18411541f7ebce8ce3cd87249a8e
Qinkuer <1165742548@qq.com>
2021-08-20 18:14:06 +0800

    Initial commit

--- app/src/main/java/com/example/ourapplication/SignIn_UIActivity.java
+++ app/src/main/java/com/example/ourapplication/SignIn_UIActivity.java
@@ -2,13 +2,44 @@ package com.example.ourapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignIn_UIActivity extends AppCompatActivity {
-
+    private TextInputEditText tietUsername;
+    private TextInputEditText tietPassword;
+    private Button bSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signinui);
+
+        this.tietUsername=findViewById(R.id.textUsername);
+        this.tietPassword=findViewById(R.id.textPassword);
+        this.bSignIn=findViewById(R.id.buttonSignIn);
+        this.bSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                
+
+
+            }
+        });
+
+
+
     }
+
+
+
+
+
 }
