fc33e8a496889e799e87d3f8ea6a47a238f3e949
Qinkuer <1165742548@qq.com>
2021-08-26 19:30:09 +0800

    Initial commit

--- app/src/main/java/com/example/ourapplication/MapFragment.java
+++ app/src/main/java/com/example/ourapplication/MapFragment.java
@@ -10,22 +10,49 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.mapapi.map.MapView;
+
 
 public class MapFragment extends Fragment {
-
     private static final String TAG = "MapFragment";
-
+    private MapView mMapView = null;
+    private View rootView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.map_fragment_layout, container, false);
-        TextView tx=rootView.findViewById(R.id.textTest1);
-        tx.setText("安康士大夫立刻就受到了看风景卢卡斯大家");
+        rootView = inflater.inflate(R.layout.map_fragment_layout, container, false);
+
+        init();
         return rootView;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+
+    private void init(){
+        mMapView = rootView.findViewById(R.id.bmapView);
+
+    }
+
+
 }
