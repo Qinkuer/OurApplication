a4d7587172f93335a0f5da20b28ad419a6728603
Qinkuer <1165742548@qq.com>
2021-08-27 19:32:49 +0800

    Initial commit

--- app/src/main/java/com/example/ourapplication/MapFragment.java
+++ app/src/main/java/com/example/ourapplication/MapFragment.java
@@ -1,5 +1,7 @@
 package com.example.ourapplication;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,13 +12,25 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 
 
 public class MapFragment extends Fragment {
     private static final String TAG = "MapFragment";
     private MapView mMapView = null;
+    private BaiduMap mBaiduMap=null;
     private View rootView;
+    private LocationClient mLocationClient;
+    private Context mContext=null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,28 +45,78 @@ public class MapFragment extends Fragment {
     }
     @Override
     public void onResume() {
-        super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
+        super.onResume();
+
     }
     @Override
     public void onPause() {
-        super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+        super.onPause();
+
     }
     @Override
     public void onDestroy() {
-        super.onDestroy();
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+
     }
+
 
 
     private void init(){
         mMapView = rootView.findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+//开启交通图
+        mBaiduMap.setTrafficEnabled(true);
+//        开启地图的定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        MyLocationConfiguration mMyLocationConfiguration=new MyLocationConfiguration(MyLocationConfiguration.LocationMode.FOLLOWING,false, BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_list_24));
+        mBaiduMap.setMyLocationConfiguration(mMyLocationConfiguration);
+        //定位初始化
+        mLocationClient = new LocationClient(this.requireContext());
 
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
+        option.setOpenAutoNotifyMode(3000,1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
     }
 
+    //构造地图数据
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            Log.e(TAG,"经纬度:"+String.valueOf(location.getLatitude())+"\t"+String.valueOf(location.getLongitude()));
+        }
+
+
+    }
 
 }
