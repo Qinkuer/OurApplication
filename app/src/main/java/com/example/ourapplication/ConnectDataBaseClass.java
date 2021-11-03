39b606fd91c82ee6cdf8125e32c21abe1cde1add
Qinkuer <1165742548@qq.com>
2021-11-03 09:03:39 +0800

    Initial commit

--- app/src/main/java/com/example/ourapplication/ConnectDataBaseClass.java
+++ app/src/main/java/com/example/ourapplication/ConnectDataBaseClass.java
@@ -5,7 +5,7 @@ package com.example.ourapplication;
 public class ConnectDataBaseClass {
     public static final String STRING_DIVER = "com.mysql.jdbc.Driver";
     public static final String STRING_URL_DATABASE = "jdbc:mysql://(IP)/SharedParking_db";
-    //    public static String url = "jdbc:mysql://116.63.172.25:3306/SharedParking_db?serverTimezone=GMT%2B8&useSSL=FALSE";
+    //    public static String url = "jdbc:mysql://(IP)/SharedParking_db?serverTimezone=GMT%2B8&useSSL=FALSE";
     public static final String STRING_DATABASE_USER = "user_sharedparking";//用户名
     public static final String STRING_DATABASE_USER_PASSWORD = "123456";//密码
 
